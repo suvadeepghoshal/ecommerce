ef92c884543886924865b1cab2f36b34e5a2b86f
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-05-04 16:01:11 +0530

    triggered workflow by using servlet

---
+++ core/src/main/java/com/ecommerceDemo/core/servlets/ExecuteWorkflow.java
@@ -0,0 +1,46 @@
+package com.ecommerceDemo.core.servlets;
+
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.model.WorkflowModel;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+@Component(service = Servlet.class)
+@SlingServletPaths(value = { "/bin/executeworkflow"})
+public class ExecuteWorkflow extends SlingSafeMethodsServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(ExecuteWorkflow.class);
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp)
+            throws ServletException, IOException {
+        String status = "Workflow Executing";
+        final ResourceResolver resourceResolver = req.getResourceResolver();
+        String payload = req.getRequestParameter("page").getString(); // in the link we are writing /bin/executeworkflow?page=
+        try {
+            if (StringUtils.isNotBlank(payload)) {
+                WorkflowSession workflowSession = resourceResolver.adaptTo(WorkflowSession.class);
+                WorkflowModel workflowModel = workflowSession.getModel("/var/workflow/models/ecommerceworkflow"); // we need to pass the path of the page where we are doing the workflow
+                WorkflowData workflowData = workflowSession.newWorkflowData("JCR_PATH", payload);
+                status = workflowSession.startWorkflow(workflowModel, workflowData).getState();
+            }
+
+        } catch (Exception e) {
+            LOG.info("\n ERROR IN WORKFLOW {} ", e.getMessage());
+        }
+        resp.setContentType("application/json");
+        resp.getWriter().write(status);
+    }
+
+}
